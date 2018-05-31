8943747af78b72fc7e15ef662f7b759e5f50032e
anhlhd00587 <anhlhd00587@fpt.edu.vn>
2018-05-31 09:52:06 +0700

    Lan dau dua file len

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            GenerateMenu();
+        }
+
+        private static List<Student> list = new List<Student>();
+
+
+        private static void AddStudent()
+        {
+            Student student = new Student();
+            Console.WriteLine("Please enter your Rollnumber:");
+            student.RollNumber = Console.ReadLine();
+            Console.WriteLine("Please enter your Name :");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("Vui lòn nhập email :");
+            student.Email = Console.ReadLine();
+            Console.WriteLine("Please enter your Phone:");
+            student.Phone = Console.ReadLine();
+            list.Add(student);
+        }
+
+        private static void SearchByName()
+        {
+            Console.WriteLine("Please enter name to search :");
+            var searchKey = Console.ReadLine();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Student student = list[1];
+                if (student.Name == searchKey)
+                {
+                    Console.WriteLine("Found:");
+                    Console.WriteLine(student.RollNumber + "-" + student.Name);
+                }
+            }
+        }
+
+        private static void DisplayStudent()
+        {
+            foreach (var student in list)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+
+        private static void GenerateMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("--------Student manager----------");
+                Console.WriteLine("1. Add new student.");
+                Console.WriteLine("2. Show list student.");
+                Console.WriteLine("3. Search student by name.");
+                Console.WriteLine("4.Exit.");
+                Console.WriteLine("---------------------");
+                Console.WriteLine("Please enter your choice :");
+                var choice = Int32.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        AddStudent();
+                        break;
+                    case 2:
+                        DisplayStudent();
+                        break;
+                    case 3:
+                        SearchByName();
+                        break;
+                    case 4:
+                        Environment.Exit(1);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        private String name;
+        private String rollNumber;
+        private String email;
+        private String phone;
+
+        public Student()
+        {
+        }
+
+        public Student(string name, string rollNumber, string email, string phone)
+        {
+            this.name = name;
+            this.rollNumber = rollNumber;
+            this.email = email;
+            this.phone = phone;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string RollNumber
+        {
+            get => rollNumber;
+            set => rollNumber = value;
+        }
+
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+
+        public string Phone
+        {
+            get => phone;
+            set => phone = value;
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
